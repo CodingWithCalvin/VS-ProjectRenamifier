ca00e32b21ecb795c422ebc183c5bb83c4c2c81a
Calvin A. Allen <calvin.allen@outlook.com>
2024-07-28 16:49:34 -0400

    WIP - stubbing out code

--- src/CodingWithCalvin.ProjectRenamifier/Commands/RenamifyProjectCommand.cs
+++ src/CodingWithCalvin.ProjectRenamifier/Commands/RenamifyProjectCommand.cs
@@ -1,8 +1,11 @@
 ﻿using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.IO;
 using System.Windows.Forms;
+using VSLangProj;
 
 namespace CodingWithCalvin.ProjectRenamifier
 {
@@ -77,27 +80,95 @@ namespace CodingWithCalvin.ProjectRenamifier
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var projectFileName = Path.GetFileName(project.FullName);
+            var oldProjectFileName = Path.GetFileName(project.FullName);
             var projectFileNameNoExtension = Path.GetFileNameWithoutExtension(project.FullName);
+            var projectExtension = Path.GetExtension(oldProjectFileName);
+
+            var newProjectFileName = $"{projectFileNameNoExtension}-NEW{projectExtension}";
+
             var projectPath =
                 Path.GetDirectoryName(project.FullName)
                 ?? throw new InvalidOperationException();
             var parentDirectoryName = new DirectoryInfo(projectPath).Parent.Name;
-            var grandparentDirectory = new DirectoryInfo(parentDirectoryName).Parent.FullName; 
+            var oldParentDirectory = new DirectoryInfo(projectPath).Parent.FullName;
+            
+            var grandparentDirectory = new DirectoryInfo(parentDirectoryName).Parent.FullName;
+            var newParentDirectory = Path.Combine(grandparentDirectory, $"{parentDirectoryName}-NEW");
+                
+            // hold onto all projects that reference this project
+            var referencingProjects = new List<Project>();
+            foreach (Project p in dte.Solution.Projects)
+            {
+                if(p.Object is VSProject referencingVSProject)
+                {
+                    foreach(Reference reference in referencingVSProject.References)
+                    {
+                        if(reference.SourceProject != null && reference.SourceProject.UniqueName == project.UniqueName)
+                        {
+                            referencingProjects.Add(p);
+                        }
+                    } 
+                }
+            }
 
+            // hold onto all projects that this project references
+            var referencedProjects = new List<Project>();
+            
+                if (project.Object is VSProject referencedVSProject)
+                {
+                    foreach (Reference reference in referencedVSProject.References)
+                    {
+                        if (reference.SourceProject != null)
+                        {
+                            referencedProjects.Add(reference.SourceProject);
+                        }
+                    }
+                }
+            
+            // unload project, then do the work
+            dte.Solution.Remove(project);
+
+            //rename parent directory, if necessary
             if (parentDirectoryName.Equals(projectFileNameNoExtension))
             {
-                // rename parent directory
+                Directory.Move(oldParentDirectory, newParentDirectory);
             }
 
             // rename project
-            
-            // fix solution?
-            // remove old project?
-            // add new project?
+            File.Move(oldProjectFileName, newProjectFileName);
 
-            // fix references?
+            // add new project back to solution
+            dte.Solution.AddFromFile(newProjectFileName, true);
+
+            // need handle to new project now that its back 
+            Project newProjectReference = null;
+            foreach (Project p in dte.Solution.Projects)
+            {
+                if(p.UniqueName == newProjectFileName)
+                {
+                    newProjectReference = p;
+                    break;
+                }
+            }
             
+            // fix projects that reference the old project to reference the new one
+            foreach (var referencingProject in referencingProjects)
+            {
+                if(referencingProject.Object is VSProject vsProject)
+                {
+                    vsProject.References.AddProject(newProjectReference);
+                }
+            }
+
+            // fix this project's references
+            foreach (var referencedProject in referencedProjects)
+            {
+                if(newProjectReference.Object is VSProject vsProject2)
+                {
+                    vsProject2.References.AddProject(referencedProject);
+                }
+            }
+                        
             // sync new namespace?
         }
     }
